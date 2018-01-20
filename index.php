9c3248c4890ce57ebac2b1b6bc36cb8aa98c1a4b
David Mackenzie <davidamackenzie@gmail.com>
2018-01-20 18:22:56 -0500

    Connect to DB test code

--- index.php
+++ index.php
@@ -11,4 +11,33 @@ $data->foo = 'bar!';
 $data->test = 'me!';
 //$data = ['foo' => 'bar', 'test' =>'me'];
 
-echo json_encode($data);
+$configs = include_once('config.php');
+
+//echo json_encode($configs);
+
+$servername = $configs['servername'];
+$username = $configs["username"];
+$password = $configs["password"];
+$dbname = $configs['dbname'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully<br />";
+
+$sql = "SELECT * FROM signup_emails";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row['email'];
+    }
+} else {
+    echo json_encode($result);
+}
+$conn->close();
