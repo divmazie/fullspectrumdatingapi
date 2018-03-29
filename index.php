ff5d3d34b273f7ac9cd085f6bcd5fe447b95cbe6
David Mackenzie <davidamackenzie@gmail.com>
2018-03-28 21:53:38 -0400

    allow fullspectrumdating.com cross origin request

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
  * Time: 20:26
  */
 
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: http://localhost:4200");
+header("Access-Control-Allow-Origin: http://fullspectrumdating.com");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
 $data = new stdClass();
 $data->foo = 'bar!';
 $data->test = 'me!';
@@ -27,17 +32,22 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully<br />";
+//echo "Connected successfully<br />";
 
 $sql = "SELECT * FROM signup_emails";
 $result = $conn->query($sql);
 
+$array = [];
+
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo $row['email'];
+        //echo $row['email'];
+        $array[] = $row['email'];
     }
+    echo json_encode($array);
 } else {
     echo json_encode($result);
 }
+
 $conn->close();
