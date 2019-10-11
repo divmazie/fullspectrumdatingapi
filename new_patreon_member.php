4af0ee7bf0ac26d6369518fb1cb19018d159eff6
David Mackenzie <davidamackenzie@gmail.com>
2019-10-11 13:42:09 -0400

    Fix typo

--- new_patreon_member.php
+++ new_patreon_member.php
@@ -16,7 +16,7 @@ error_log('POST = ' . json_encode($_POST));
 $key = 'FD1409BC355C9AB81A24E62452164B81B1724C76B8CF17EDE065CFACF7C3483D';
 
 if ($_POST['email'] && $_POST['key'] && $_POST['key']==$key) {
-    $email = $_POST['emai'];
+    $email = $_POST['email'];
     $signupEmail = SignupEmail::newSignupEmail($email);
     $signupEmail->invite();
     error_log('Invite sent to '.$email);
