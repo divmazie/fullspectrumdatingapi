e521c53b4b9977f5e86e15fafc54039e61c8c9c4
David Mackenzie <davidamackenzie@gmail.com>
2019-10-11 13:36:34 -0400

    Logs on new patreon

--- new_patreon_member.php
+++ new_patreon_member.php
@@ -10,6 +10,9 @@ spl_autoload_register(function ($class_name) {
     include $class_name . '.php';
 });
 
+error_log('new_patreon_member.php touched');
+error_log('POST = ' . json_encode($_POST));
+
 $key = 'FD1409BC355C9AB81A24E62452164B81B1724C76B8CF17EDE065CFACF7C3483D';
 
 if ($_POST['email'] && $_POST['key'] && $_POST['key']==$key) {
